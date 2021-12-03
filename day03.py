bd9b4b25161ef88617fbbfd31d3cb9a39d415b8e
Tim Conceivable <37424061+timconceivable@users.noreply.github.com>
2021-12-03 10:53:56 -0600

    refactor

--- day03.py
+++ day03.py
@@ -6,16 +6,13 @@ def input_to_list(filename):
     file.close()
     return data_list
 
-def convert_to_decimal(binary):
-    return int(binary, 2)
-
 def find_gamma(data_list):
     data_len = len(data_list)
     binary_len = len(data_list[0])
     gamma_list = [0]*binary_len
     for binary in data_list:
         for i in range(binary_len):
-            if binary[i] is "1":
+            if binary[i] == "1":
                 gamma_list[i] += 1
     gamma_binary = ""
     for n in gamma_list:
@@ -26,9 +23,9 @@ def find_gamma(data_list):
 def find_epsilon(binary):
     new_binary = ""
     for i in binary:
-        if i is "1":
+        if i == "1":
             new_binary += "0"
-        elif i is "0":
+        elif i == "0":
             new_binary += "1"
     return new_binary
 
@@ -37,12 +34,12 @@ def main():
     x = "_"
     diagnostic = input_to_list(r'day03-input.txt')
     gamma = find_gamma(diagnostic)
-    gamma_decimal = convert_to_decimal(gamma)
     epsilon = find_epsilon(gamma)
-    epsilon_decimal = convert_to_decimal(epsilon)
-    power = gamma_decimal*epsilon_decimal
-    print("PART 1: \n   gamma rate: ", gamma, "  (", gamma_decimal, ")",
-        "\n   epsilon rate: ", epsilon, "  (", epsilon_decimal, ")",
+    gamma_dec = int(gamma, 2)
+    epsilon_dec = int(epsilon, 2)
+    power = gamma_dec*epsilon_dec
+    print("PART 1: \n   gamma rate: ", gamma_dec, "  (", gamma, ")",
+        "\n   epsilon rate: ", epsilon_dec, "  (", epsilon, ")",
         "\n   power consumption: ", power, sep="")
 
     #PART 2
